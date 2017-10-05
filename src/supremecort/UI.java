391bbc054fca794ee81d31444ce9e12bc3c38095
KaguilarA <kevin.aguilar@interaction.cr>
2017-10-05 09:27:47 -0600

    Seed code

---
+++ src/supremecort/UI.java
@@ -0,0 +1,16 @@
+package supremecort;
+
+/**
+ *
+ * @author Kevin Aguilar && Eduardo Cruz
+ */
+public class UI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
